91390619bff4ca5a6b635dcf37f03941ac1d6f81
bkp5 <48831618+bkp5@users.noreply.github.com>
2020-04-18 13:22:52 +0530

    product details section updated.

--- app/src/main/java/bkp/com/ecommerce2/ProductDetailsActivity.java
+++ app/src/main/java/bkp/com/ecommerce2/ProductDetailsActivity.java
@@ -3,12 +3,32 @@ package bkp.com.ecommerce2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ProductDetailsActivity extends AppCompatActivity {
+
+    private FloatingActionButton addToCartBtn;
+    private ImageView productImage;
+    private ElegantNumberButton numberButton;
+    private TextView productName, productDescription, productPrice;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
+
+        addToCartBtn = (FloatingActionButton)findViewById(R.id.add_product_to_cart_btn);
+        productImage = (ImageView)findViewById(R.id.product_image_details);
+        numberButton = (ElegantNumberButton)findViewById(R.id.number_btn);
+        productName = (TextView)findViewById(R.id.product_name_details);
+        productDescription = (TextView)findViewById(R.id.product_description_detail);
+        productPrice = (TextView)findViewById(R.id.product_price_details);
+
+
     }
 }
